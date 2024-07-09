d203786b8f67cab3d0b610368e15ed7751b0a623
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-07-09 22:28:37 +0900

    스프링!

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,16 @@ import hello.core.Member.Grade;
 import hello.core.Member.Member;
 import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,21 +1,30 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import hello.core.Member.Grade;
 import hello.core.Member.Member;
 import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void test() {
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
         //when
         memberService.join(member);
-        Member findMember = memberService.findMember(2L);
+        Member findMember = memberService.findMember(1L);
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
