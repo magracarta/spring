91e4fb22704822d90ba33c403f59d9aec99527de
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-03-17 10:39:58 +0900

    .

---
+++ ykStpirngBootVersionUp/src/main/java/com/yk/ykstpirngbootversionup/YkStpirngBootVersionUpApplication.java
@@ -0,0 +1,13 @@
+package com.yk.ykstpirngbootversionup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkStpirngBootVersionUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YkStpirngBootVersionUpApplication.class, args);
+    }
+
+}
---
+++ ykStpirngBootVersionUp/src/test/java/com/yk/ykstpirngbootversionup/YkStpirngBootVersionUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yk.ykstpirngbootversionup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YkStpirngBootVersionUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
