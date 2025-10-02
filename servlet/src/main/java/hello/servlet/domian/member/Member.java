be1b738c2ab4f791abaa43ec7468786b429d1e8d
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-10-02 15:24:18 +0900

    서블릿으로 html 전달

---
+++ servlet/src/main/java/hello/servlet/domian/member/Member.java
@@ -0,0 +1,15 @@
+package hello.servlet.domian.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
