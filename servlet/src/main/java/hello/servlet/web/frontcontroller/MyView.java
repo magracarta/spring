80c29eaa3cbd6468e67d70690fb55d0296d924e2
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-10-09 16:31:24 +0900

    v2~v3

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher(viewPath).forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+        this.modelToRequestAttribute(model, request);
+        this.render(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach(request::setAttribute);
+    }
+
+}
