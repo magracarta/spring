6e4fc0a73c9262c83e9b750afd33f37d5fbaf153
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2025-11-09 21:38:24 +0900

    Controller  정리완료

--- servlet/src/main/java/hello/servlet/web/springMvc/old/MyHttpRequestHandler.java
+++ servlet/src/main/java/hello/servlet/web/springMvc/old/MyHttpRequestHandler.java
@@ -5,8 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.HttpRequestHandler;
-
 import java.io.IOException;
+
 @Component("/springmvc/request-handler")
 public class MyHttpRequestHandler implements HttpRequestHandler {
     @Override
