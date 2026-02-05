7f8b86297f6a20774074189272d4a391646a4f91
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2026-02-05 22:21:47 +0900

    요청매핑

--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,13 +1,14 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
-    private static final Logger log = LoggerFactory.getLogger(LogTestController.class);
 
     @RequestMapping("/log-test")
     public String logTest() {
@@ -15,11 +16,14 @@ public class LogTestController {
 
         System.out.println("name = " + name);
 
+        //+ 텍스트로 하게 되면 문자열 + 문자열로 연산을 하게 되어 쓸데없는 메모리 낭비가 된다 .
+        // 해서 + 보다는 {} 로 바인딩 해주는게 좋다.
+        //log.trace("trace ="+name);
         log.trace("trace ={}",name);
         log.debug("debug ={}",name);
-        log.info("name={}", name);
-        log.warn("name={}", name);
-        log.error("name={}", name);
+        log.info("info={}", name);
+        log.warn("warn={}", name);
+        log.error("error={}", name);
 
         return "ok";
     }
---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -0,0 +1,126 @@
+package hello.springmvc.basic.requestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class MappingController {
+    /**
+     * 기본 요청
+     * 둘다 허용 /hello-basic, /hello-basic/
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     * 스프링 부트 3.0 부터는 /hello-basic , /hello-basic/ 는 서로 다른 URL 요청을 사용해야 한다.
+     */
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("hello-basic");
+        return "ok";
+    }
+
+    /**
+     * method 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션 (코드보기)
+     *
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    /**
+     * pathVariable 사용
+     * 변수명이 같으면 생략 가능
+     *
+     * @return
+     * @PathVariavle("userId") String userId -> @pathVariable userId
+     * /mapping/useA
+     */
+    @GetMapping(value = "/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String userId) {
+        log.info("mappingPath= {}", userId);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! = )
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! = )
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*" 
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+
+}
